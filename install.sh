9cc9dcfaafe156b6eb58a54cee0ad3c8ac1c81f6
VerusC <veruscoin2024github@gmail.com>
2024-11-09 18:29:20 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,23 +19,15 @@ wget https://raw.githubusercontent.com/VerusC/VerusCliMining/main/config_luckpoo
 
 chmod +x ~/ccminer/ccminer
 
-cat << EOF > ~/ccminer/start.sh
-#!/bin/sh
-~/ccminer/ccminer -c ~/ccminer/config.json
-EOF
-chmod +x start.sh
+
 
 cat << EOF > ~/ccminer/start2.sh
 #!/bin/sh
 ~/ccminer/ccminer -c ~/ccminer/config_luckpool.json
 EOF
-chmod +x start2.sh
+chmod +x start.sh
 
-cat << EOF > ~/ccminer/start3.sh
-#!/bin/sh
-~/ccminer/ccminer -c ~/ccminer/config_zerg_party.json
-EOF
-chmod +x start3.sh
+
 
 echo "setup nearly complete."
 echo "Edit the config with \"nano ~/ccminer/config.json\""
